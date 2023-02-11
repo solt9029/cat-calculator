c49e9df464b25ba9cb949cb9a52a287098ec113f
Kenshi Shiode <atemu1212@yahoo.co.jp>
2023-02-11 10:52:21 +0900

    Add state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import { Grid, Button } from '@mui/material'
 import "./App.css"
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <Grid container>
       <Grid container style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-50%)"}}>
         <Grid item xs={12} sm={5} my={4} style={{display: "grid", placeItems: "center"}}>
           <div style={{textAlign: "center"}}>
-            <Button variant="contained" style={{fontSize: "2rem"}}>＋</Button>
+            <Button variant="contained" style={{fontSize: "2rem"}} onClick={() => {
+              if (count < 7) { setCount(count + 1); }
+            }}>＋</Button>
           </div>
         </Grid>
         <Grid item xs={12} sm={2} my={4} style={{display: "grid", placeItems: "center"}}>
           <div style={{textAlign: "center"}}>
-            <h1 style={{fontSize: "5rem"}}>2</h1>
+            <h1 style={{fontSize: "5rem"}}>{count}</h1>
           </div>
         </Grid>
         <Grid item xs={12} sm={5} my={4} style={{display: "grid", placeItems: "center"}}>
           <div style={{textAlign: "center"}}>
-            <Button variant="contained" style={{fontSize: "2rem"}}>ー</Button>
+            <Button variant="contained" style={{fontSize: "2rem"}} onClick={() => {
+              if (count > 0) { setCount(count - 1); }
+            }}>ー</Button>
           </div>
         </Grid>
       </Grid>
